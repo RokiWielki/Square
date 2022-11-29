98beacec158696f33f23fdf373c0d62db177252f
RokiWielki <Cruck222@interia.pl>
2022-11-29 15:04:08 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//Napisz program, który wyświetla na ekranie konsoli „kwadrat” zbudowany ze znaku
+//„*”. Liczbę wierszy (a tym samym kolumn) ma podać użytkownik.
+
+using System;
+
+namespace Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give side length: ");
+            int a=int.Parse(Console.ReadLine());
+            Square(a);
+        }
+        static void Square(int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
